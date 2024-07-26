060f0ec0a1227da7bf87bc4e3c8ecfc38ca3e84e
wj0624 <wonjeong4538@gmail.com>
2024-07-26 12:12:57 +0900

    Design: 폰트, 로고 파일 추가 및 메인 페이지 구성

--- app/src/main/java/com/heendoongs/coordibattle/MainActivity.kt
+++ app/src/main/java/com/heendoongs/coordibattle/MainActivity.kt
@@ -3,14 +3,17 @@ package com.heendoongs.coordibattle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var logo : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         window.setFlags(
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
-
     }
 }
